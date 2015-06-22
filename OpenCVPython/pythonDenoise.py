d6a1850d936eff0a264c9d5b4de18ffd3e254cbf
jjromphf <jromphf@library.rochester.edu>
2015-06-22 16:31:08 -0400

    Fixed ffmpeg subprocess call

--- OpenCVPython/pythonDenoise.py
+++ OpenCVPython/pythonDenoise.py
@@ -17,15 +17,12 @@ args = vars(parser.parse_args())#Get some variables from parse_args dictionary (
 directory = args['directory']
 output = args['output']
 
-
-def imageDenoise():
+def imageDenoise(): #Find all the files in the working directory -- in this case looking for .tif extension only
     for dirname, dirnames, filenames in os.walk(directory):
         for file in filenames:
             if '.tif' in file:
-            
-                filePath =  os.path.abspath(os.path.join(dirname, file))
                 
-                global outputDirectory
+                filePath =  os.path.abspath(os.path.join(dirname, file))
             
                 outputDirectory = output + file
                 
@@ -36,46 +33,49 @@ def imageDenoise():
                 img = cv2.imread(filePath)
                 
                 if img is not None:
-                    
+                    #Print a message once the image matrix is loaded
                     print workingMessage
-                    
+                    #track how long it takes to denoise
                     start_time = time.time()
-
+                    #Input matrix, output matrix, strength for luminance component, strength for chrominance component, template patch in pixels, window size for weighted average of pixels
                     dst = cv2.fastNlMeansDenoisingColored(img,None,3,10,7,21)
-
+                    #write the denoised file and report on it
                     cv2.imwrite(outputDirectory, dst)
 
                     print completeMessage
 
                     print("%s seconds" % (time.time() - start_time))
+
+    return outputDirectory
     
     print 'Starting ffmpeg'
-
-def outputInfo():
+#Handler for input and output directory arguments being passed to ffmpegTranscode function
+def outputInfo(outputDirectory):
     
     os.chdir(output)
-    global newDirectory
-    global newOutput
     strippedDirectory = outputDirectory[:-9]
     inputExtension = '%5d.tif'
     outputExtension = 'access.mp4'
     newDirectory = strippedDirectory + inputExtension
     newOutput = strippedDirectory + outputExtension
 
-    return newDirectory, newOutput
+    return (newDirectory, newOutput)
+#Transcode the sequence using libx264
+def ffmpegTranscode(newDirectory, newOutput):
 
-def ffmpegTranscode():
-    subprocess.call(['ffmpeg', '-i', newDirectory, '-c:v', 'libx264', '-crf', '18', '-r', '18', '-an', '-pix_fmt', 'yuv420p', newOutput ])
+    ffmpegCommand = ['ffmpeg', '-i', newDirectory , '-c:v', 'libx264', '-crf', '18', '-r', '18', '-an', '-pix_fmt', 'yuv420p', newOutput ]
+
+    subprocess.call(ffmpegCommand)
 
 
 #The program
 
 
-imageDenoise()
+denoiser = imageDenoise()
 
-outputInfo()
+newDirectory, newOutput = outputInfo(denoiser)
 
-ffmpegTranscode()
+ffmpegTranscode(newDirectory, newOutput)
 
 
 
